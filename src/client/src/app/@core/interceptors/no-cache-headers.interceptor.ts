e7f1f013e5b4faaf6767ef80994e3398cfd6261b
Tharles de Sousa Andrade <irtharles@gmail.com>
2022-04-18 19:37:10 -0300

    update documentation

--- src/client/src/app/@core/interceptors/no-cache-headers.interceptor.ts
+++ src/client/src/app/@core/interceptors/no-cache-headers.interceptor.ts
@@ -4,8 +4,6 @@ import { Injectable } from '@angular/core';
 export class NoCacheHeaders implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         const authReq = req.clone({
-            // Prevent caching in IE, in particular IE11.
-            // See: https://support.microsoft.com/en-us/help/234067/how-to-prevent-caching-in-internet-explorer
             setHeaders: {
                 'Cache-Control': 'no-cache',
                 Pragma: 'no-cache'
