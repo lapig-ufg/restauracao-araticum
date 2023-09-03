070c6c8e3847a9ef77fc44e26a22e05d5815f70b
Jairo Matos da Rochal <devjairomr@gmail.com>
2023-09-02 22:08:46 -0300

    update-camadas

--- src/client/src/environments/environment.ts
+++ src/client/src/environments/environment.ts
@@ -1,7 +1,14 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+let commitId = ''
+try{
+  const config = require('../../../../version.json');
+  console.log(config)
+  commitId = config.commitId
+}catch{
+  console.log('Not version')
+}
 export const environment = {
   APP: "ARATICUM",
   APP_NAME: 'Araticum - Articulação pela Restauração do Cerrado',
@@ -13,7 +20,8 @@ export const environment = {
   OWS_O2: "https://o2.lapig.iesa.ufg.br/ows",
   OWS_O3: "https://o3.lapig.iesa.ufg.br/ows",
   OWS_O4: "https://o4.lapig.iesa.ufg.br/ows",
-  APP_URL: 'https://atlas-homolog.lapig.iesa.ufg.br'
+  APP_URL: 'https://atlas-homolog.lapig.iesa.ufg.br',
+  COMMIT_ID:commitId
 };
 
 /*
