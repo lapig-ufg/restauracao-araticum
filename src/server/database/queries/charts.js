070f5b162f7299498e1162cce00ea1bdf4d683da
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2024-09-10 00:02:49 -0300

    geom

--- src/server/database/queries/charts.js
+++ src/server/database/queries/charts.js
@@ -54,7 +54,7 @@ module.exports = function (app) {
             {
                 source: 'lapig',
                 id: 'restoration',
-                sql: " SELECT  CAST(st_area(st_union(geometry)::geography)/10000 as double precision) as value "
+                sql: " SELECT  CAST(st_area(st_union(geom)::geography)/10000 as double precision) as value "
                     + " FROM araticum_restauracao_2024 a "
                     + " WHERE " + regionFilter
             }
@@ -84,7 +84,7 @@ module.exports = function (app) {
             {
                 source: 'lapig',
                 id: 'areaRestorationPerProject',
-                sql: "SELECT metprinc as label, st_area(st_union(geometry)::geography)/10000 as value from araticum_restauracao_2024 GROUP BY 1 ORDER BY 2 DESC ",
+                sql: "SELECT metprinc as label, st_area(st_union(geom)::geography)/10000 as value from araticum_restauracao_2024 GROUP BY 1 ORDER BY 2 DESC ",
                 mantain: true
             }
         ];
@@ -111,7 +111,7 @@ module.exports = function (app) {
             {
                 source: 'lapig',
                 id: 'projetos',
-            sql: "SELECT projeto, fonte, st_area(st_union(geometry)::geography)/10000  as value from araticum_restauracao_2024 GROUP BY 1, 2 ORDER BY 3 DESC ",
+            sql: "SELECT projeto, fonte, st_area(st_union(geom)::geography)/10000  as value from araticum_restauracao_2024 GROUP BY 1, 2 ORDER BY 3 DESC ",
                 mantain: true
             }
         ]
