f6ec03a40010362f6d5559c2f2128f390d662f13
Guilherme Freire <guilherme.fspe@gmail.com>
2026-09-11 10:59:18 -0300

    Fix: Correção de "biome" para "bioma"

--- src/server/controllers/download.js
+++ src/server/controllers/download.js
@@ -84,8 +84,8 @@ module.exports = function (app) {
             builder.addFilter('cd_geocmu', "'" + region.value + "'");
         } else if (region.type === 'state') {
             builder.addFilter('uf', "'" + region.value + "'");
-        } else if (region.type === 'biome') {
-            builder.addFilter('biome', "'" + region.value + "'");
+        } else if (region.type === 'bioma') {
+            builder.addFilter('bioma', "'" + region.value + "'");
         }
 
         if (filter !== undefined) {
